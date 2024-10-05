ab6fb351dc079a4e471174ace2faa21db2b3c9ec
Jaspreet Malak <39964866+JassM13@users.noreply.github.com>
2024-10-04 22:43:52 -0700

    Updates...

---
+++ src/kernel/vga/mod.rs
@@ -0,0 +1,4 @@
+pub mod text;
+
+// Future graphics modules can be added here
+// pub mod graphics;
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
+
+mod kernel;
 
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
+use kernel::vga::text::print_something;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    print_something();
 
     loop {}
 }
 
-/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
